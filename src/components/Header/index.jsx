fc70f4097eb0f9fd1a5a319dcf78cd96ec4f7074
SumitFox9 <sumit@kodnest.com>
2023-11-20 11:25:04 +0530

    theme added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import data from "../../data/languages";
-function index({ setLanguage, setValue }) {
+import { FaMoon, FaRegSun } from "react-icons/fa";
+function index({ setLanguage, setTheme, theme }) {
+  console.log("Theme", theme);
   return (
-    <div className="px-3 bg-black/90 py-6 flex items-center gap-12 justify-between">
+    <div
+      className={`px-3 ${
+        theme === "vs-dark" ? "bg-[#00030B]" : "bg-gray-300/90"
+      } py-6 flex items-center gap-12 justify-between`}
+    >
       <div className="flex items-center gap-12">
-        <a href="/" className="text-white text-2xl font-semibold">
+        <a
+          href="/"
+          className={`${
+            theme !== "vs-dark" ? "text-[#00030B]" : "text-white"
+          }  text-2xl font-semibold`}
+        >
           KodIDE
         </a>
         {/* <div className="flex items-center gap-6">
@@ -18,18 +29,33 @@ function index({ setLanguage, setValue }) {
           </select>
         </div> */}
       </div>
-      <select
-        name="languages"
-        id="languages"
-        onChange={(e) => {
-          setLanguage(e.target.value);
-        }}
-        className="w-[210px] py-1"
-      >
-        {data["languages"].map((language) => {
-          return <option value={language.id}>{language.name}</option>;
-        })}
-      </select>
+
+      <div className="flex items-center gap-6">
+        <button
+          className="w-8 h-8 bg-white flex items-center justify-center shadow-2xl rounded-md"
+          onClick={() => {
+            if (theme === "vs-dark") {
+              setTheme("vs-light");
+            } else {
+              setTheme("vs-dark");
+            }
+          }}
+        >
+          {theme !== "vs-dark" ? <FaMoon /> : <FaRegSun />}
+        </button>
+        <select
+          name="languages"
+          id="languages"
+          onChange={(e) => {
+            setLanguage(e.target.value);
+          }}
+          className="w-[210px] py-1 bg-purple-600 text-white"
+        >
+          {data["languages"].map((language) => {
+            return <option value={language.id}>{language.name}</option>;
+          })}
+        </select>
+      </div>
     </div>
   );
 }
